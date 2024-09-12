1d698a84eb85c0ce7240ddd1030cb9c0b03bc188
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-09-13 00:25:04 +0700

    8-1-Seven-Segment-2-Digits

--- managed_components/SEVENSEGMENT/main.cpp
+++ managed_components/SEVENSEGMENT/main.cpp
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include "SevenSegment.h"
 
-SevenSegment s1(0);
-SevenSegment s2(4);
+SevenSegment s1(0);  // Seven Segment ตัวแรก
+SevenSegment s2(4);  // Seven Segment ตัวที่สอง
 
 extern "C" void app_main(void)
 {
     while (1)
     {
+        // แสดงเลข 1 บน Seven Segment ตัวแรก
         s1.DisplayNumber(1);
         s1.DisplayOn();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        s1.DisplayOff();
+        vTaskDelay(500 / portTICK_PERIOD_MS); // หน่วงเวลา 500ms
 
+        s1.DisplayOff(); // ปิดการแสดงผลบน Seven Segment ตัวแรก
+
+        // แสดงเลข 5 บน Seven Segment ตัวที่สอง
         s2.DisplayNumber(5);
         s2.DisplayOn();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        s2.DisplayOff();
+        vTaskDelay(500 / portTICK_PERIOD_MS); // หน่วงเวลา 500ms
+
+        s2.DisplayOff(); // ปิดการแสดงผลบน Seven Segment ตัวที่สอง
     }
 }
